5ebf3972505a2af09437485f9e1acd79992a0023
Andrew S. Lentz <Andrew.S.Lentz@gmail.com>
2016-12-22 14:23:04 -0600

    main page finished, router built

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import background from './background.png';
+import {Link} from 'react-router';
 import andrew from './Andrew.png';
+import medium from './medium.png';
+import twitter from './Twitter.png';
+import github from './GitHub.png';
+import linkedin from './linkedin.png';
+import './Resume.pdf'
 import './App.css';
 
 class App extends Component {
@@ -8,13 +13,19 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>{'{' + 'Andrew S. Lentz' + '}'}</h2>
-          <img src={andrew} alt='me!' />
+          <h2>{'{Andrew S. Lentz}'}</h2>
+          <img className='andrew' src={andrew} alt='me!' />
           <h3>Front End Engineer</h3>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='shadowBox'>Hi, I’m Andrew, and I am a new developer from Minneapolis, MN. I am passionate about creative problem solving involved in implementing new features and designs. I am proficient in JavaScript, including frameworks like React and jQuery, as well as HTML/CSS, and git. All my sites are fully dynamic. I am a quick study and always eager to learn new technologies! I am available for hire or freelance work.</div>
+        <div className='navigation'><Link to='/portfolio'><p>Portfolio</p></Link><a href='./Resume.pdf'><p>Resume</p></a></div>
+        <div className='contact'><p className='margin'>952-913-1436</p><p>Andrew.S.Lentz@gmail.com</p></div>
+        <div className='socials'>
+          <a href='https://github.com/AndrewSLentz'><img className='socials' src={github} alt='github' /></a>
+          <a href='https://www.linkedin.com/in/andrew-s-lentz-03931799'><img className='socials' src={linkedin} alt='linkedin' /></a>
+          <a href='https://medium.com/@andrew.s.lentz'><img className='medium' src={medium} alt='medium' /></a>
+          <a href='https://twitter.com/AndrewSLentz'><img className='socials' src={twitter} alt='twitter' /></a>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './App';
+import Portfolio from './Portfolio.js';
+import NoMatch from './NoMatch.js';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+    <Route path="portfolio" component={Portfolio}/>
+    <Route path="*" component={NoMatch}/>
+  </Router>
+), document.getElementById('root'));
