8790fe899cacae0641e74a1bde0e535b0421b1a2
Andrew S. Lentz <Andrew.S.Lentz@gmail.com>
2016-12-22 17:12:06 -0600

    fixed nomatch in deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
+import { useBasename } from 'history'
 import App from './App';
 import Portfolio from './Portfolio.js';
 import NoMatch from './NoMatch.js';
 import './index.css';
 
 ReactDOM.render((
-  <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
+  <Router history={useBasename(() => browserHistory)({ basename: process.env.PUBLIC_URL })} onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
     <Route path="/" component={App} />
     <Route path="portfolio" component={Portfolio}/>
     <Route path="*" component={NoMatch}/>
